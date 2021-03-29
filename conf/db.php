036c8c86e92befe76fa1531f6e82a5d10bfc4514
root <root@iu7.ru>
2021-03-29 13:28:48 -0400

    check style

--- conf/db.php
+++ conf/db.php
@@ -5,14 +5,13 @@
 */
 class DB
 {
-
 	const USER = "root";
 	const PASS = 1234;
 	const HOST = "localhost";
 	const DB   = "file_hosting";
 
 	public static function connToDB() 
-    {
+	{
 		$user = self::USER;
 		$pass = self::PASS;
 		$host = self::HOST;
