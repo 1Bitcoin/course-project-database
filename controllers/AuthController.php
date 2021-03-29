414ed90acdf6cc945abe223e2fcc39e989d82922
root <root@iu7.ru>
2021-03-29 13:27:24 -0400

    check style

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,13 +6,13 @@ require_once(VIEW_PATH . 'AuthView.php');
 class AuthController extends Controller 
 {
 	public function __construct() 
-    {
+	{
 		$this->model = new AuthModel();
 		$this->view = new AuthView();
 	}
 
 	public function login() 
-    {
+	{
 		$this->pageData['title'] = "Главная страница";
 		$this->view->render($this->pageData);
 	}
--- controllers/Controller.php
+++ controllers/Controller.php
@@ -7,7 +7,7 @@ class Controller
 	protected $pageData = array();
 
 	public function __construct() 
-    {
+	{
 		$this->view = new View();
 		$this->model = new Model();
 	}	
