d2bb21bbf28c6db19d4cd6a60132ebbc04b25857
root <root@iu7.ru>
2021-03-29 13:35:21 -0400

    check style

--- conf/route.php
+++ conf/route.php
@@ -2,37 +2,36 @@
 
 class Routing 
 {
-	public static function run() 
-	{
+    public static function run() 
+    {
+        // Контроллер и его метод по умолчанию
+        $controllerName = "IndexController";
+        $action = "indexPage";
 
-		// Контроллер и его метод по умолчанию
-		$controllerName = "IndexController";
-		$action = "indexPage";
 
+        // Получить имя необходимого контроллера
+        $route = explode("/", $_SERVER['REQUEST_URI']);
 
-		// Получить имя необходимого контроллера
-		$route = explode("/", $_SERVER['REQUEST_URI']);
+        if (empty($route)) 
+        {
+            $controllerName = ucfirst($route[1] . "Controller");
+        }
 
-		if (empty($route)) 
-		{
-			$controllerName = ucfirst($route[1] . "Controller");
-		}
+        // Подключить файл нужного контроллера
+        require_once(CONTROLLER_PATH . $controllerName . ".php"); 
 
-		// Подключить файл нужного контроллера
-		require_once(CONTROLLER_PATH . $controllerName . ".php"); 
+        // Получить имя необходимого метода
+        if (isset($route[2])) 
+        {
+            $action = $route[2];
+        }
 
-		// Получить имя необходимого метода
-		if (isset($route[2])) 
-		{
-			$action = $route[2];
-		}
+        $controller = new $controllerName();
+        $controller->$action();
+    }
 
-		$controller = new $controllerName();
-		$controller->$action();
-	}
+    public function errorPage() 
+    {
 
-	public function errorPage() 
-	{
-
-	}
+    }
 }
