a2d580232cf78b07618460f0ae96b9b78dec6945
1Bitcoin <shigalkin@mail.ru>
2021-05-09 09:43:38 -0400

    add new tests

--- tests/LoginModelTest.php
+++ tests/LoginModelTest.php
@@ -2,23 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 
-define("ROOT", '/var/www/course-project-database');
-define("COMPONENT_BASE", ROOT . "/components/base/");
-define("COMPONENT_ERROR", ROOT . "/components/error/");
-define("COMPONENT_FILE", ROOT . "/components/file/");
-define("COMPONENT_INDEX", ROOT . "/components/index/");
-define("COMPONENT_UPLOAD", ROOT . "/components/upload/");
-define("COMPONENT_LOGIN", ROOT . "/components/login/");
-define("COMPONENT_LOGOUT", ROOT . "/components/logout/");
-define("COMPONENT_REGISTER", ROOT . "/components/register/");
-define("COMPONENT_DOWNLOAD", ROOT . "/components/download/");
-define("COMPONENT_MAIN", ROOT . "/components/");
-
-define("REPOSITORY", ROOT . "/repository/");
-define("CONNECTION", ROOT . "/repository/connection/");
-
-define("UPLOAD_PATH", ROOT . "/uploaded_files/");
-define("CONFIG_DATABASE", ROOT . "/repository/config/DataBase.txt");
+require_once("/var/www/course-project-database/config/Config.php");
 
 require_once(COMPONENT_LOGIN . 'LoginModel.php');
 require_once(CONNECTION . 'Connection.php');
@@ -27,6 +11,35 @@ require_once(ROOT . '/service/Logger.php');
 class LoginModelTest extends TestCase
 { 
     protected $result;
+
+    public function testLoginUserInvalidPassword()
+    {
+        $logger = $this->getMockBuilder(Logger::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $userRepository = $this->getMockBuilder(UserRepository::class)
+        ->setMethods(['checkExistsUser'])
+        ->setMethods(['checkCoincidenceUser'])
+        ->getMock();
+
+        $result['nums'] = 1;
+        $userRepository->expects($this->once())
+        ->method('checkExistsUser')
+        ->will($this->returnValue($result));
+
+        $userRepository->expects($this->once())
+        ->method('checkCoincidenceUser')
+        ->will($this->returnValue($result));
+
+        $loginModel = new LoginModel($userRepository, 1);
+        $loginModel->logger = $logger;
+
+        $infoUser['email'] = "567";
+        $infoUser['hash_password'] = "hj";
+        $infoUser['ip'] = "45.45.45.45";
+        $this->assertEquals("Неверный пароль!", $loginModel->loginUser($infoUser)['errors']);
+    }
 
     public function testAddLogNoLogger()
     {
@@ -94,10 +107,6 @@ class LoginModelTest extends TestCase
         ->disableOriginalConstructor() 
         ->getMock();
 
-        $connection = $this->getMockBuilder(Connection::class)
-        ->disableOriginalConstructor() 
-        ->getMock();
-
         $userRepository = $this->getMockBuilder(UserRepository::class)
         ->setMethods(['checkExistsUser'])
         ->getMock();
@@ -109,45 +118,10 @@ class LoginModelTest extends TestCase
 
         $loginModel = new LoginModel($userRepository, 1);
         $loginModel->logger = $logger;
-        $loginModel->connection = $connection;
 
         $infoUser['email'] = "567";
         $infoUser['hash_password'] = "hj";
         $infoUser['ip'] = "45.45.45.45";
         $this->assertEquals("Пользователь не зарегестрирован!", $loginModel->loginUser($infoUser)['errors']);
-    }
-
-    public function testLoginUserInvalidPassword()
-    {
-        $logger = $this->getMockBuilder(Logger::class)
-        ->disableOriginalConstructor()
-        ->getMock();
-
-        $connection = $this->getMockBuilder(Connection::class)
-        ->disableOriginalConstructor() 
-        ->getMock();
-
-        $userRepository = $this->getMockBuilder(UserRepository::class)
-        ->setMethods(['checkExistsUser'])
-        ->setMethods(['checkCoincidenceUser'])
-        ->getMock();
-
-        $result['nums'] = 1;
-        $userRepository->expects($this->once())
-        ->method('checkExistsUser')
-        ->will($this->returnValue($result));
-
-        $userRepository->expects($this->once())
-        ->method('checkCoincidenceUser')
-        ->will($this->returnValue($result));
-
-        $loginModel = new LoginModel($userRepository, 1);
-        $loginModel->logger = $logger;
-        $loginModel->connection = $connection;
-
-        $infoUser['email'] = "567";
-        $infoUser['hash_password'] = "hj";
-        $infoUser['ip'] = "45.45.45.45";
-        $this->assertEquals("Неверный пароль!", $loginModel->loginUser($infoUser)['errors']);
     }
 }
